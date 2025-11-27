7b5b7d5e6161f8d6300ebf10073abf637871e2a6
Dhanush <dhanushkumar614@gmail.com>
2025-11-27 18:45:47 -0500

    Initial project setup: created gateway, wallet-service, order-service, added docker-compose infra, added health endpoints

---
+++ order-service/src/main/java/com/paycart/order/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.paycart.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "ORDER OK";
+    }
+}
